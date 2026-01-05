21be464efb9742c13c2e2120f4c7048bdcc0f671
Ratikant Sahoo <ratikant.sahoo@siemens.com>
2026-01-05 15:23:07 +0530

    chore: add ihire-dashboard application

--- apps/ihire-dashboard/federation.config.js
+++ apps/ihire-dashboard/federation.config.js
@@ -1,10 +1,10 @@
 const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
-  name: 'ihire-host',
+  name: 'ihire-dashboard',
 
-  remotes: {
-    'ihire-dashboard': 'http://localhost:4201/remoteEntry.json',
+  exposes: {
+    './routes': './apps/ihire-dashboard/src/app/app.routes.ts',
   },
 
   shared: {
--- apps/ihire-host/src/app/app.routes.ts
+++ apps/ihire-host/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => loadRemoteModule('ihire-dashboard', './routes').then((m) => m.routes),
+  },
+];
