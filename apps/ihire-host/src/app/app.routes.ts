b59809b8a1500b5e919d099efa6f0634e13aed90
Ratikant Sahoo <ratikant.sahoo@siemens.com>
2026-01-05 15:47:29 +0530

    chore: add ihire-requisition application

--- apps/ihire-host/src/app/app.routes.ts
+++ apps/ihire-host/src/app/app.routes.ts
@@ -6,4 +6,8 @@ export const routes: Routes = [
     path: 'dashboard',
     loadChildren: () => loadRemoteModule('ihire-dashboard', './routes').then((m) => m.routes),
   },
+  {
+    path: 'requisitions',
+    loadChildren: () => loadRemoteModule('ihire-requisition', './routes').then((m) => m.routes),
+  },
 ];
